17262b167ac8b6f882909980d9935cf16b7f0315
saintcitto <saintcitto@icloud.com>
2025-10-30 03:31:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-=import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Synth, Loop, Transport } from "tone";
 import { formatTimeForInput } from "./utils/helpers";
 import KTVErrorBoundary from "./components/KTVErrorBoundary";
