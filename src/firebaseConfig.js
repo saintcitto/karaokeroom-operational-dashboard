3d89afb67c1874cbe2fad2112606e01365487a56
saintcitto <saintcitto@icloud.com>
2025-10-30 05:55:23 +0700

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set, onValue, remove, update, push } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,5 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-
+export const auth = getAuth(app);
 export { ref, set, onValue, remove, update, push };
